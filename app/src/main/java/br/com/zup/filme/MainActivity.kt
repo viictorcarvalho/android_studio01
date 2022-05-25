58a1950427c6e2a9cfeb59283897f255bdb06f08
viictorcarvalho <you@example.com>
2022-05-25 16:36:39 -0300

    Criação do segundo botão

--- app/src/main/java/br/com/zup/filme/MainActivity.kt
+++ app/src/main/java/br/com/zup/filme/MainActivity.kt
@@ -1,11 +1,28 @@
 package br.com.zup.filme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botaoMensagem = findViewById<Button>(R.id.botao_mensagem)
+
+        botaoMensagem.setOnClickListener {
+            Toast.makeText(this, "Filme incrível", Toast.LENGTH_LONG).show()
+        }
+
+        val botaoDetalheFilme = findViewById<Button>(R.id.botao_detalhe_filme)
+
+        botaoDetalheFilme.setOnClickListener {
+            val intent = Intent(this, DetalhesFilme::class.java)
+            startActivity(intent)
+
+        }
     }
 }
