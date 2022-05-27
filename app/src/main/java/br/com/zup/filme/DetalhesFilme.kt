38cda7af28c2e79d4af3db84ce1b0fa509e400b9
viictorcarvalho <you@example.com>
2022-05-26 21:58:58 -0300

    Implementação de Callback's

--- app/src/main/java/br/com/zup/filme/DetalhesFilme.kt
+++ app/src/main/java/br/com/zup/filme/DetalhesFilme.kt
@@ -1,11 +1,44 @@
 package br.com.zup.filme
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class DetalhesFilme : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(MainActivity.TAG, "Passou pelo onCreate() do detalhe")
         setContentView(R.layout.activity_detalhes_filme)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity.TAG, "Passou pelo onStart() do detalhe")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity.TAG, "Passou pelo onResume() do detalhe")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MainActivity.TAG, "Passou pelo onPause() do detalhe")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity.TAG, "Passou pelo onStop() do detalhe")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity.TAG, "Passou pelo onDestroy() do detalhe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity.TAG, "Passou pelo onRestart() do detalhe")
     }
 }
--- app/src/main/java/br/com/zup/filme/MainActivity.kt
+++ app/src/main/java/br/com/zup/filme/MainActivity.kt
@@ -3,26 +3,70 @@ package br.com.zup.filme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "APP_FILME"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "Passou pelo onCreate() da main")
         setContentView(R.layout.activity_main)
 
         val botaoMensagem = findViewById<Button>(R.id.botao_mensagem)
-
         botaoMensagem.setOnClickListener {
-            Toast.makeText(this, "Filme incrível", Toast.LENGTH_LONG).show()
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/html"
+            intent.putExtra(Intent.EXTRA_EMAIL, "emaildo@diretor.com")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Assunto")
+            intent.putExtra(Intent.EXTRA_TEXT, "Eu sou o corpo do email")
+
+            startActivity(intent)
         }
 
         val botaoDetalheFilme = findViewById<Button>(R.id.botao_detalhe_filme)
-
         botaoDetalheFilme.setOnClickListener {
             val intent = Intent(this, DetalhesFilme::class.java)
+            val bundle = Bundle()
+            val nomeDoFilme = "Interstellar"
+            bundle.putString("nomeDoFilme", nomeDoFilme)
+            intent.putExtra("Message", bundle)
             startActivity(intent)
-
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Passou pelo onStart() da main")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Passou pelo onResume() da main")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Passou pelo onPause() da main")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Passou pelo onStop() da main")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Passou pelo onDestroy() da main")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Passou pelo onRestart() da main")
     }
 }
